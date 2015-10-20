242330fd89f1e701f2fa6bb540144e44ad8bc1cc
bergonom <jberghoff@gmail.com>
2015-10-19 23:49:24 -0700

    Improved display of filtered items.

--- contentscript.js
+++ contentscript.js
@@ -1,22 +1,20 @@
-var sitesToBlock = ["Walmart","SheIn","Groupon","Talbots","Micro","Perfumania","A4C","Venus","Ashford","Jos.","FreedomPop","Sponsored"];
-var $itemTexts = $(".size1of2 .content-body, .size1of2 .sponsored-by");
+var sitesToBlock = ["Walmart","SheIn","Groupon","Talbots","Micro","Perfumania","A4C","Venus","Ashford","Jos.","FreedomPop","Sponsored","Outback","Logan's","Ann Taylor", "Men's Wearhouse", "Vitamin Shoppe","Adorama","Olive Garden","via eBay","Gymboree"];
+var $itemTexts = $(".size1of2 .content-body");
 
-sitesToBlock = $.map(sitesToBlock, function(n,i){return n.toLowerCase();});
-
+var sitesToBlockLower = $.map(sitesToBlock, function(n,i){return n.toLowerCase();});
+console.log("$itemTexts = " + $itemTexts.length);
 $itemTexts.each(function() {
 	var $currentEl = $(this);
 	var thisText = $(this).text().trim().toLowerCase();
 	var firstWords = thisText.split(" ");
 	var $outerBox, dealTitle;
 	
-	$.each(sitesToBlock, function(index, value) {
-		console.log("sitesToBlock[index] = " + sitesToBlock[index]);
-		console.log("thisText = " + thisText);
-		if(thisText.indexOf(sitesToBlock[index]) >= 0) {
+	$.each(sitesToBlockLower, function(index, value) {	
+		if(thisText.indexOf(sitesToBlockLower[index]) >= 0) {
 			$outerBox = $currentEl.closest(".content-box");
 			dealTitle = $outerBox.find(".headline-xlarge").html();
 			$outerBox.wrapInner("<div style='display:none'></div>");
-			$outerBox.prepend("Filtered: " + dealTitle + "");
+			$outerBox.prepend("Filtered (" + sitesToBlock[index] + "): " + dealTitle + "");
 			$outerBox.css("height", "16px").css("padding", "8px");
 			return false;
 		}
