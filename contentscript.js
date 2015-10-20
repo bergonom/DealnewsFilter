2ba88895c4453d371f15c68abf1c60163ea28198
bergonom <jberghoff@gmail.com>
2015-10-19 23:24:57 -0700

    Now including sponsored deal text.

--- contentscript.js
+++ contentscript.js
@@ -1,23 +1,22 @@
 var sitesToBlock = ["Walmart","SheIn","Groupon","Talbots","Micro","Perfumania","A4C","Venus","Ashford","Jos.","FreedomPop","Sponsored"];
-var $itemTexts = $(".size1of2 .content-body");
-var numWordsToCheck = 15;
+var $itemTexts = $(".size1of2 .content-body, .size1of2 .sponsored-by");
 
-// For some reason there are a lot of blank tokens in each DN description
-var numBlankStrings = 20;
-numWordsToCheck += numBlankStrings;
+sitesToBlock = $.map(sitesToBlock, function(n,i){return n.toLowerCase();});
 
 $itemTexts.each(function() {
 	var $currentEl = $(this);
-	var thisText = $(this).text().trim();
+	var thisText = $(this).text().trim().toLowerCase();
 	var firstWords = thisText.split(" ");
 	var $outerBox, dealTitle;
 	
 	$.each(sitesToBlock, function(index, value) {
+		console.log("sitesToBlock[index] = " + sitesToBlock[index]);
+		console.log("thisText = " + thisText);
 		if(thisText.indexOf(sitesToBlock[index]) >= 0) {
 			$outerBox = $currentEl.closest(".content-box");
 			dealTitle = $outerBox.find(".headline-xlarge").html();
 			$outerBox.wrapInner("<div style='display:none'></div>");
-			$outerBox.prepend("Blocked: " + dealTitle + "");
+			$outerBox.prepend("Filtered: " + dealTitle + "");
 			$outerBox.css("height", "16px").css("padding", "8px");
 			return false;
 		}
